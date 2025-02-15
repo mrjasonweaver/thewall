6662477814cada6a3575c943f35564762a2f13b0
Jason Weaver <indyplanets@gmail.com>
2025-02-15 12:03:55 -0600

    stub out functions for updating player and adding player.

--- public/script.js
+++ public/script.js
@@ -3,35 +3,103 @@ const ctx = canvas.getContext("2d");
 
 const ws = new WebSocket("ws://localhost:3000");
 
+const gameState = {
+  players: [],
+  connections: 0,
+};
+
 // Set circle properties
 const centerX = canvas.width / 2;
 const centerY = canvas.height / 2;
-const radius = 5;
+const radius = 10;
 
 ws.onopen = () => {
   console.log("Connected to WebSocket server");
-  // Start a new path
-  ctx.beginPath();
-  // Draw the circle
-  ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
-  // Stroke the circle (draw outline)
-  ctx.stroke();
-  // Optional: Fill the circle
-  // ctx.fillStyle = 'blue';
-  // ctx.fill();
+  gameState.connections++;
+  console.log(gameState);
 };
 
-ws.onmessage = (event) => {
-  const data = JSON.parse(event.data);
-  // Handle game updates, e.g., redraw objects based on received data
-  console.log(data);
-  const player = data.playerId;
-  //ctx.clearRect(0, 0, canvas.width, canvas.height);
+/**
+ * Add a new player to game.
+ * @param {object} player - The player data to be added.
+ */
+const addPlayer = (player) => {
+  gameState.players.push(player);
+  addPlayerToCanvas(player);
+};
+
+/**
+ * Update player game state.
+ * @param {object} playerData - The player data to be updated.
+ */
+const updatePlayer = (playerData) => {
+  gameState.players = gameState.players.map((player) => {
+    if (player.playerId === playerData.playerId) {
+      return playerData;
+    } else {
+      return player;
+    }
+  });
+  addPlayerToCanvas(playerData);
+};
+
+/**
+ * Add player to canvas.
+ * @param {object} player - The player to add to the canvas.
+ */
+const addPlayerToCanvas = (player) => {
   // Start a new path
   ctx.beginPath();
   // Draw the circle
-  ctx.arc(data.x, data.y, radius, 0, 2 * Math.PI);
+  ctx.arc(player.data?.x, player.data?.y, radius, 0, 2 * Math.PI);
   ctx.stroke();
+};
+
+/**
+ * Clear game canvas.
+ */
+const clearGameCanvas = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+/**
+ * Check new player.
+ * @param {object} data - Player data.
+ * @reutrns {boolean} Whether the player is new.
+ */
+const checkPlayerIsNew = (data) => {
+  let newPlayer;
+  gameState.players.forEach((player) => {
+    if (player.playerId !== data.playerId) {
+      newPlayer = player;
+    }
+  });
+  return !!newPlayer;
+};
+
+// Handle game updates, e.g., redraw objects based on received data
+ws.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+  const players = gameState.players;
+  let newPlayer;
+
+  if (players.length) {
+    // Clear game canvas.
+    clearGameCanvas();
+    // Check if player is new.
+    newPlayer = checkPlayerIsNew(data);
+  } else {
+    // No players yet.
+    newPlayer = true;
+  }
+
+  if (newPlayer) {
+    addPlayer(data);
+  } else {
+    updatePlayer(data);
+  }
+
+  console.log(gameState.players);
 };
 
 ws.onclose = () => {
